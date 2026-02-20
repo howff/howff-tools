7887e03f7d842d51abe4a6b7142c6d282f86bbcd
howff <3064316+howff@users.noreply.github.com>
2026-02-20 13:45:38 +0000

    login.sh - fix tmux new passing SSHPASS, add debugging

--- bin/login.sh
+++ bin/login.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-# Display menu of hosts and login using password taken from ansible vault.
-# Uses one tmux session per host.
+
+# Use -d or -v for debugging
+if [ "$1" == "-d" -o "$1" == "-v" ]; then
+        debug=1
+fi
+
+# Configuration:
+hosts=("nsh-fs02 (for S3)"  "nsh-rc-desktop01"  "nsh-gpu-desktop01"  "nsh-gpu-swarm01"  "smi-webs01 (for SemEHR/RGO container)"  "smi-edris-db01 (for SemEHR/nshdr postgres)"  "$(id -un)@smi-data-mgmt01 (to extract)"  "$(id -un)@smi-edris-db01 (for cohort)"  "smi-edris-db01 (for cohort)")
 
 # Use the content of this file as the password to decrypt the vault:
 export ANSIBLE_VAULT_PASSWORD_FILE=/home/smi/smi_vault_password
@@ -8,28 +14,29 @@ export ANSIBLE_VAULT_PASSWORD_FILE=/home/smi/smi_vault_password
 # Decrypt the vault and grab the password
 # view outputs two lines, second line is vault_ansible_become_password: "blah"
 export SSHPASS=$(ansible-vault view src/deploy/inventories/nsh/group_vars/all/vault | sed -n '/password/s/^.*"\(.*\)"/\1/p')
+if [ "$debug" != "" ]; then echo SSHPASS=$SSHPASS; fi
 
-hosts=("nsh-fs02 (for S3)"  "nsh-rc-desktop01"  "nsh-gpu-desktop01"  "nsh-gpu-swarm01"  "smi-webs01 (for SemEHR/RGO container)"  "smi-edris-db01 (for SemEHR/nshdr
- postgres)"  "$(id -un)@smi-data-mgmt01 (to extract)"  "$(id -un)@smi-edris-db01 (for cohort)")
 n=0
 for hostlabel in "${hosts[@]}"; do
     echo " ${n} = ${hostlabel}"
     n=$((n+1))
 done
+
 printf "Enter number : "
 read num
+
 hostname=${hosts[$num]%% *}
 username=${hosts[$num]%%@*}
 
-# if no username in host then prepend agans-smi otherwise use the logged-in username
+# if no username in host then prepend agans-smi
 if [ "$username" == "${hosts[$num]}" ]; then
     hostname="agans-smi@${hostname}"
 fi
 
-# Start a new tmux session or reuse an existing one
-# Session name will be username@hostname
 if tmux list-sessions | grep "${hostname}: " > /dev/null; then
+    if [ "$debug" != "" ]; then echo tmux attach -t ${hostname}; read enter; fi
     tmux attach -t ${hostname}
 else
-    tmux new -s ${hostname} sshpass -e -v ssh ${hostname}
+    if [ "$debug" != "" ]; then echo tmux new -s ${hostname} sshpass -e -v ssh ${hostname}; read enter; fi
+    tmux new -s ${hostname} -e SSHPASS=$SSHPASS sshpass -e -v ssh ${hostname}
 fi
